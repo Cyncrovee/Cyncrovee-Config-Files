3d433b20d58c69d2e5022b3f04311c5a285b0a54
Cyncrovee <cyncrovee@yahoo.com>
2025-03-09 13:42:09 +0000

    feat: update statusline.lua

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -1,27 +1,65 @@
 -- Init modeVar global variable to avoid "modeVar not found" error
 vim.g.modeVar = ""
+vim.g.fileSizeVar = ""
 
+function getMode()
+  -- Set modeVar to the correct value depending on the current mode
+  -- This does not cover all modes
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == "n" then
+    vim.g.modeVar = "NORMAL"
+  elseif mode == "i" then
+   vim.g.modeVar = "INSERT"
+  elseif mode == "v" then
+    vim.g.modeVar = "VISUAL-CHAR"
+  elseif mode == "V" then
+    vim.g.modeVar = "VISUAL-LINE"
+  elseif mode == "CTRL-V" then
+    vim.g.modeVar = "VISUAL-BLOCK"
+  elseif mode == "s" then
+    vim.g.modeVar = "SELECT-CHAR"
+  elseif mode == "s" then
+    vim.g.modeVar = "SELECT-LINE"
+  elseif mode == "R" then
+    vim.g.modeVar = "REPLACE"
+  elseif mode == "c" then
+    vim.g.modeVar = "COMMAND"
+  elseif mode == "cv" then
+    vim.g.modeVar = "EX"
+  else
+    vim.g.modeVar = "NOT FOUND"
+  end
+end
+
+function getFileSize()
+  -- Get the file size from the current buffer
+  vim.g.fileSizeVar = vim.fn.getfsize(vim.api.nvim_buf_get_name(0))
+end
+
+-- Call getMode() and getFileSize() after entering a buffer
+-- Call getFileSize() after setting the modified var for a buffer
+-- Call getMode() when the mode changes
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    getMode()
+    getFileSize()
+  end
+})
+vim.api.nvim_create_autocmd("BufModifiedSet", {
+  callback = function()
+    getFileSize()
+  end
+})
 vim.api.nvim_create_autocmd("ModeChanged", {
   callback = function()
-    -- Set modeVar to the correct value depending on the current mode
-    -- This does not cover all modes
-    if vim.api.nvim_get_mode().mode == "n" then
-      vim.g.modeVar = "NORMAL"
-    elseif vim.api.nvim_get_mode().mode == "i" then
-      vim.g.modeVar = "INSERT"
-    elseif vim.api.nvim_get_mode().mode == "v" then
-      vim.g.modeVar = "VISUAL"
-    elseif vim.api.nvim_get_mode().mode == "V" then
-      vim.g.modeVar = "VISUAL-LINE"
-    else
-      vim.g.modeVar = "NOT FOUND"
-    end
+    getMode()
+    getFileSize()
   end
 })
 
 local function statusLine()
   -- Return the statusline content
-  return ("%f%y%m%r%=%{modeVar}%=%l:%c")
+  return ("%f %y%m%r %Ll:%{fileSizeVar}b %=%{modeVar}|Buf-%n%=%l:%c %p%% ")
 end
 
 -- Set the statusbar to the return value of the statusLine function
