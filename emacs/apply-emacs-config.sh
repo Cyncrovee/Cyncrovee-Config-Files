e55797515596fa92dd8d4d6b611ed9df2c72a74a
Cyncrovee <cyncrovee@yahoo.com>
2025-03-12 16:31:24 +0000

    feat: update emacs script

--- emacs/apply-emacs-config.sh
+++ emacs/apply-emacs-config.sh
@@ -1,1 +1,6 @@
-cp ~/.dotfiles/emacs/init.el ~/.emacs.d/
+#!/bin/bash
+
+# This will only work if the directory is named ".dotfiles"
+mkdir ~/.config/emacs
+cp ~/.dotfiles/emacs/init.el ~/.config/emacs/
+echo '(load "~/.config/emacs/init.el")' >> ~/.emacs.d/init.el
