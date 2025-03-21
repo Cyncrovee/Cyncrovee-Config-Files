4b324464f6b6cf3f096fb7ca3f5725b5aa41585f
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-03-21 20:00:11 +0000

    feat: update emacs config, add "README-Emacs.org" file

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -4,9 +4,6 @@
   (add-to-list 'load-path "<path where use-package is installed>")
   (require 'use-package))
 
-;; ELPA Theme(s)
-(use-package solarized-theme
-  :ensure t)
 ;; ELPA Packages
 (use-package org
   :ensure t)
@@ -18,6 +15,10 @@
   :ensure t)
 (use-package embark
   :ensure t)
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode))
 (use-package indent-bars ;; See: https://github.com/jdtsmith/indent-bars (GPL-3.0)
   :ensure t
   :hook ((lsp-mode org-mode) . indent-bars-mode))
@@ -52,25 +53,30 @@
 (use-package doom-themes
   :ensure t)
 ;; MELPA Packages
-(use-package evil-collection ;; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
-  :after evil
-  :ensure t
-  :init
-  (evil-collection-init))
-(use-package find-file-in-project
-  :ensure t)
-(use-package lsp-mode ;; See: https://github.com/emacs-lsp/lsp-mode (GPL-3.0)
+(use-package lsp-mode                            ;; See: https://github.com/emacs-lsp/lsp-mode (GPL-3.0)
   :ensure t
   :hook (rust-ts-mode . lsp-deferred)
   :commands (lsp-deferred))
 (use-package lsp-ui
   :ensure t)
+(use-package evil-collection                     ;; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
+  :after evil
+  :ensure t
+  :init
+  (evil-collection-init))
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode))
 (setq-default doom-modeline-indent-info t)       ;; Show information about the indentation settings
 (setq-default doom-modeline-total-line-number t) ;; Show the total amount of lines in the buffer
 (setq-default doom-modeline-workspace-name t)    ;; Show workspace name
+(use-package dashboard                           ;; See https://github.com/emacs-dashboard/emacs-dashboard (GPL-3.0)
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+(setq dashboard-items '((recents  . 10)
+                        (projects . 5)
+                        (agenda   . 5)))
 
 ;; Load theme
 (load-theme 'doom-opera) ;; Feel free to swap out with another theme
@@ -80,6 +86,8 @@
 (global-display-line-numbers-mode) ;; Enable line numbers
 (global-hl-line-mode)              ;; Highlight current line
 (column-number-mode)               ;; Display line number/column
+(electric-pair-mode)               ;; Automatically close parens
+(electric-quote-mode)              ;; Automatically close quotes
 ;; (completion-preview-mode)       ;; Enable autosuggestions (Only available in Emacs 30.1 and up)
 
 ;; Set options
