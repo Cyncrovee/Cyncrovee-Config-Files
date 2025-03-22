11caf5f706cec8076e96f7ff509aba0dd0d23bbf
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-03-22 17:15:55 +0000

    feat: update emacs config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,4 +1,4 @@
-;; Setup use-package (see: https://github.com/jwiegley/use-package, GPL-3.0)
+;;; Package Managment
 ;; Setup use-package (See: https://github.com/jwiegley/use-package, GPL-3.0)
 (eval-when-compile
   (add-to-list 'load-path "<path where use-package is installed>")
@@ -6,8 +6,6 @@
 
 ;; ELPA Packages
 (use-package org
-  :ensure t)
-(use-package markdown-mode
   :ensure t)
 (use-package magit
   :ensure t)
@@ -19,10 +17,10 @@
   :ensure t
   :init
   (projectile-mode))
-(use-package indent-bars ;; See: https://github.com/jdtsmith/indent-bars (GPL-3.0)
+(use-package indent-bars ; See: https://github.com/jdtsmith/indent-bars (GPL-3.0)
   :ensure t
   :hook ((lsp-mode org-mode) . indent-bars-mode))
-(use-package evil ;; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
+(use-package evil        ; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
   :ensure t
   :init
   (setq evil-want-keybinding nil)
@@ -39,27 +37,25 @@
   :ensure t
   :init
   (marginalia-mode))
-(use-package orderless ;; See: https://github.com/oantolin/orderless (GPL-3.0)
+(use-package orderless   ; See: https://github.com/oantolin/orderless (GPL-3.0)
   :ensure t
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
-
 ;; Setup MELPA stable (See: https://stable.melpa.org/#/getting-started and https://github.com/melpa/melpa, GPL-3.0)
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://stable.melpa.org/packages/") t)
-
 ;; MELPA Theme(s)
 (use-package doom-themes
   :ensure t)
 ;; MELPA Packages
-(use-package lsp-mode                            ;; See: https://github.com/emacs-lsp/lsp-mode (GPL-3.0)
+(use-package lsp-mode                            ; See: https://github.com/emacs-lsp/lsp-mode (GPL-3.0)
   :ensure t
   :hook (rust-ts-mode . lsp-deferred)
   :commands (lsp-deferred))
 (use-package lsp-ui
   :ensure t)
-(use-package evil-collection                     ;; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
+(use-package evil-collection                     ; See: https://github.com/emacs-evil/evil (GPL-3.0) & https://github.com/emacs-evil/evil-collection (GPL-3.0)
   :after evil
   :ensure t
   :init
@@ -67,33 +63,33 @@
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode))
-(setq-default doom-modeline-indent-info t)       ;; Show information about the indentation settings
-(setq-default doom-modeline-total-line-number t) ;; Show the total amount of lines in the buffer
-(setq-default doom-modeline-workspace-name t)    ;; Show workspace name
-(use-package dashboard                           ;; See https://github.com/emacs-dashboard/emacs-dashboard (GPL-3.0)
+(setq-default doom-modeline-indent-info t)       ; Show information about the indentation settings
+(setq-default doom-modeline-total-line-number t) ; Show the total amount of lines in the buffer
+(setq-default doom-modeline-workspace-name t)    ; Show workspace name
+(use-package dashboard                           ; See https://github.com/emacs-dashboard/emacs-dashboard (GPL-3.0)
   :ensure t
   :config
   (dashboard-setup-startup-hook))
+(setq dashboard-center-content t)
 (setq dashboard-items '((recents  . 10)
                         (projects . 5)
                         (agenda   . 5)))
 
+;;; General Options
 ;; Load theme
-(load-theme 'doom-opera) ;; Feel free to swap out with another theme
-
+(load-theme 'doom-opera) ; Feel free to swap out with another theme
 ;; Enable mode(s)
-(global-auto-revert-mode)          ;; Automatically refresh file
-(global-display-line-numbers-mode) ;; Enable line numbers
-(global-hl-line-mode)              ;; Highlight current line
-(column-number-mode)               ;; Display line number/column
-(electric-pair-mode)               ;; Automatically close parens
-(electric-quote-mode)              ;; Automatically close quotes
-;; (completion-preview-mode)       ;; Enable autosuggestions (Only available in Emacs 30.1 and up)
-
+(global-auto-revert-mode)          ; Automatically refresh file
+(global-display-line-numbers-mode) ; Enable line numbers
+(global-hl-line-mode)              ; Highlight current line
+(column-number-mode)               ; Display line number/column
+(electric-pair-mode)               ; Automatically close parens
+(electric-quote-mode)              ; Automatically close quotes
+;; (completion-preview-mode)       ; Enable autosuggestions (Only available in Emacs 30.1 and up)
 ;; Set options
-(setq-default make-backup-files nil) ;; Disable backup files
-(setq-default auto-save-default nil) ;; Disable auto save files
-(setq-default create-lockfiles nil)  ;; Disable lock file creation
-(setq-default tab-width 4)           ;; Set tab spaces to 4
-(setq-default indent-tabs-mode nil)  ;; Convert tabs to spaces
-;; (setq visible-bell t)             ;; Stops error sounds, however may enable a screen flashing/flickering effect
+(setq-default make-backup-files nil) ; Disable backup files
+(setq-default auto-save-default nil) ; Disable auto save files
+(setq-default create-lockfiles nil)  ; Disable lock file creation
+(setq-default tab-width 4)           ; Set tab spaces to 4
+(setq-default indent-tabs-mode nil)  ; Convert tabs to spaces
+;; (setq visible-bell t)             ; Stops error sounds, however may enable a screen flashing/flickering effect
