fe706c786c68b6f735c4a644448c5d084ee1c5e1
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-05-28 14:41:37 +0100

    feat: add custom functions and prefix key descriptions

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,7 +1,7 @@
-;;; Enable modes
-(which-key-mode)
+;;; Enable modes/misc functions
 (electric-pair-mode)
 (electric-quote-mode)
+(which-key-mode)
 
 ;;; Set options
 (setopt
@@ -83,39 +83,68 @@
 ;;; Setup Eglot
 (add-hook 'rust-mode-hook 'eglot-ensure)
 
+;;; Setup custom functions
+(defun open-emacs-config ()
+  (interactive)
+  (dired user-emacs-directory))
+(defun evil-write-quit ()
+  (interactive)
+  (evil-write)
+  (evil-quit))
+
 ;;; Setup keymaps
 ;;; General
 (evil-set-leader 'normal (kbd "SPC")) ; Set leader key
 (evil-global-set-key 'normal (kbd "<leader> SPC") 'execute-extended-command) ; Double tap space for M-x functionality (I think)
-(evil-global-set-key 'normal (kbd "<leader> wo") 'other-window) ; Move to another window
-(evil-global-set-key 'normal (kbd "<leader> jl") 'join-line) ; Move to another window
+(which-key-add-key-based-replacements "<leader> g" "General")
+(evil-global-set-key 'normal (kbd "<leader> gw") 'other-window) ; Move to another window
+(evil-global-set-key 'normal (kbd "<leader> gl") 'join-line) ; Connect current line to above line
+(evil-global-set-key 'normal (kbd "<leader> gc") 'open-emacs-config) ; Opent the config directory in dired
+(evil-global-set-key 'normal (kbd "<leader> gt") 'swap-theme) ; Hot swap between spacemacs themes
 ;;; Evil
-(evil-global-set-key 'normal (kbd "<leader> ew") 'evil-write)
+(which-key-add-key-based-replacements "<leader> e" "Evil")
+(which-key-add-key-based-replacements "<leader> ew" "Evil Write")
+(evil-global-set-key 'normal (kbd "<leader> eww") 'evil-write)
+(evil-global-set-key 'normal (kbd "<leader> ewq") 'evil-write-quit)
+(which-key-add-key-based-replacements "<leader> eq" "Evil Quit")
+(evil-global-set-key 'normal (kbd "<leader> eqq") 'evil-quit)
+(evil-global-set-key 'normal (kbd "<leader> eqa") 'evil-quit-all)
 (evil-global-set-key 'normal (kbd "<up>") 'evil-window-up)
 (evil-global-set-key 'normal (kbd "<down>") 'evil-window-down)
 (evil-global-set-key 'normal (kbd "<left>") 'evil-window-left)
 (evil-global-set-key 'normal (kbd "<right>") 'evil-window-right)
 ;;; Magit
 (evil-global-set-key 'normal (kbd "<f9>") 'magit) ; Use magit with F9
-;; Dired
-(evil-global-set-key 'normal (kbd "<leader> dr") 'make-directory) ; Use magit with F9
-(evil-global-set-key 'normal (kbd "<leader> df") 'make-empty-file) ; Use magit with F9
+;;; Files/Directories
+(which-key-add-key-based-replacements "<leader> c" "Files/Directories")
+(evil-global-set-key 'normal (kbd "<leader> cd") 'make-directory)
+(evil-global-set-key 'normal (kbd "<leader> cf") 'make-empty-file)
+;;; Describe
+(which-key-add-key-based-replacements "<leader> d" "Describe")
+(evil-global-set-key 'normal (kbd "<leader> ds") 'describe-symbol)
+(evil-global-set-key 'normal (kbd "<leader> dk") 'describe-key)
+(evil-global-set-key 'normal (kbd "<leader> dm") 'describe-key)
 ;;; Sly
+(which-key-add-key-based-replacements "<leader> d" "Sly")
 (evil-global-set-key 'normal (kbd "<leader> sl") 'sly)
 (evil-global-set-key 'normal (kbd "<leader> sb") 'sly-eval-buffer)
 (evil-global-set-key 'normal (kbd "<leader> sf") 'sly-eval-defun)
 (evil-global-set-key 'visual (kbd "<leader> sr") 'sly-eval-region)
 ;;; Eglot
+(which-key-add-key-based-replacements "<leader> d" "Elgot")
 (evil-global-set-key 'normal (kbd "<leader> lf") 'eglot-format-buffer)
 (evil-global-set-key 'normal (kbd "<leader> la") 'eglot-code-actions)
 ;;; Rust
+(which-key-add-key-based-replacements "<leader> d" "Rust")
 (evil-global-set-key 'normal (kbd "<leader> rr") 'rust-run)
 ;;; Menus
-(evil-global-set-key 'normal (kbd "<leader> bm") 'buffer-menu)
+(which-key-add-key-based-replacements "<leader> u" "Menus")
+(evil-global-set-key 'normal (kbd "<leader> ub") 'buffer-menu)
 ;;; Tabs
-(evil-global-set-key 'normal (kbd "<leader> tn") 'tab-bar-new-tab)
+(which-key-add-key-based-replacements "<leader> t" "Tabs")
 (evil-global-set-key 'normal (kbd "<leader> tn") 'tab-bar-new-tab)
 (evil-global-set-key 'normal (kbd "<leader> tc") 'tab-bar-close-tab)
 (evil-global-set-key 'normal (kbd "<leader> to") 'tab-bar-close-other-tabs)
 ;;; Modes
+(which-key-add-key-based-replacements "<leader> m" "Modes")
 (evil-global-set-key 'normal (kbd "<leader> mo") 'olivetti-mode)
