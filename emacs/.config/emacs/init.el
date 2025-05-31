db2df3daabeb7c06fb2b6ee0268762d8b1953921
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-05-31 16:04:16 +0100

    feat: add light/dark theme functions for startup

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,8 +1,15 @@
-(use-package spacemacs-theme
-  :ensure t
-  :defer t
-  :init
-  (load-theme 'spacemacs-dark))
+(defun load-dark-theme()
+  (use-package spacemacs-theme
+    :ensure t
+    :defer t
+    :init
+    (load-theme 'spacemacs-dark)))
+
+(defun load-light-theme()
+  (load-theme 'modus-operandi-tinted))
+
+(load-dark-theme) ; If you want the light theme, comment out this line and uncomment the below line
+;;; (load-light-theme)
 
 ;;; Load elisp files
 (load-file (concat user-emacs-directory "/lisp/functions.el"))
