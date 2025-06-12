2464f97369dd90d9cc207fe29b1681f6ad46a9c4
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-06-12 21:31:00 +0100

    docs: change formatting for user theme config information

--- emacs/.config/emacs/lisp/user.el
+++ emacs/.config/emacs/lisp/user.el
@@ -1,7 +1,11 @@
 ;;; User-Specific Configuration
 (setopt
  ;; This variable (user-boot-theme) sets the theme on boot.
- ;; Set to "light" for the ef-light theme, "summer" for the ef-summer theme, "dark" for the ef-owl theme, or "winter" for the ef-winter theme.
+ ;; The following options are available:
+ ;; "light": ef-light
+ ;; "summer": ef-summer
+ ;; "dark": ef-owl
+ ;; "winter": ef-winter
  ;; Any other value will not load a theme, allowing you to load your own.
  ;; Alternatively, comment out the line to show a theme selection promt at boot.
  user-boot-theme "dark"
