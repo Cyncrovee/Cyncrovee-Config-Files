ae801d7f9632c0526892412c9dfe0b1f4226d779
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-06-12 22:40:33 +0100

    feat: add more user-boot-theme options

--- emacs/.config/emacs/lisp/user.el
+++ emacs/.config/emacs/lisp/user.el
@@ -3,9 +3,12 @@
  ;; This variable (user-boot-theme) sets the theme on boot.
  ;; The following options are available:
  ;; "light": ef-light
+ ;; "spring": ef-spring
  ;; "summer": ef-summer
  ;; "dark": ef-owl
+ ;; "autumn": ef-autumn
  ;; "winter": ef-winter
+ ;; "bio": ef-bio
  ;; Any other value will not load a theme, allowing you to load your own.
  ;; Alternatively, comment out the line to show a theme selection promt at boot.
  user-boot-theme "dark"
